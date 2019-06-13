48ac27038d426b3453c4e68a144fa8cbc5243b82
Matthew Weaver <matt@techplicity.co.uk>
2019-06-13 14:04:30 +0100

    Initial code for assessing the secretary problem.

---
+++ main.py
@@ -0,0 +1,27 @@
+from ApplicantPool import ApplicantPool
+
+
+
+
+new_tuple = ()
+
+if new_tuple:
+    print('full')
+else:
+    print('empty')
+
+
+
+
+
+
+
+
+
+
+# Create a new applicant pool.
+app_pool = ApplicantPool(20)
+
+
+pass
+
---
+++ test_applicantPool.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+
+class TestApplicantPool(TestCase):
+
+    def test_look_and_leap_list_sizes(self):
+        self.fail()
+
+    def test_create_look_list(self):
+        self.fail()
+
+    def test_create_leap_list(self):
+        self.fail()
+
+    def test_top_applicant_processing(self):
+        self.fail()
+
+    def test_chosen_applicant_processing(self):
+        self.fail()
