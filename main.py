02e50fc48dbd725c5063c588c1a1139ea637d60c
Matthew Weaver <31797463+Wibbo@users.noreply.github.com>
2020-09-08 05:52:03 +0100

    Working out the best way to display results.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import plotly_express as px
 # Setup the output screen.
 st.write('# Recruitment with optimal stopping')
 
+
 st.sidebar.markdown('')
 st.sidebar.image('./images/Obj1.png', width=300)
 st.sidebar.markdown('## Application parameters')
@@ -16,8 +17,8 @@ show_details = st.sidebar.checkbox('Show details')
 hire_last = st.sidebar.checkbox('Hire last applicant?')
 st.write('')
 st.sidebar.markdown('## MORE INFORMATION')
-st.sidebar.markdown("<a href='https://www.matt-matters.co.uk/post/copy-of-recruiting-in-a-rush-read-on'>What is optimal stopping</a>", unsafe_allow_html=True)
-st.sidebar.markdown("<a href='https://github.com/Wibbo/Optimal-stopping'>The code is on GitHub</a>", unsafe_allow_html=True)
+st.sidebar.markdown("<a href='https://bit.ly/3jVfHpz'>What is optimal stopping</a>", unsafe_allow_html=True)
+st.sidebar.markdown("<a href='https://bit.ly/2RhTohL'>The code is on GitHub</a>", unsafe_allow_html=True)
 st.sidebar.markdown("<a href='https://www.objectivity.co.uk'>Objectivity web site</a>", unsafe_allow_html=True)
 st.sidebar.markdown("Email mweaver@objectivity.co.uk")
 
@@ -28,14 +29,14 @@ top_applicant_last = 0
 offered_to_last = 0
 offers_made = 0
 
+# Create and process each campaign.
 campaigns = [Campaign(number_of_applicants, count, hire_last) for count in range(number_of_cycles)]
 df = pd.DataFrame.from_records([s.to_dict() for s in campaigns])
-
 average_interviews = df['offered_to_index'].mean()
-
 
 candidate_data = pd.DataFrame(columns=('campaign', 'index', 'best chosen', 'value', 'offered'))
 
+# Determine summary data.
 for i in campaigns:
     if i.offered_to_value == 0:
         top_applicant_chosen += 1
@@ -59,9 +60,7 @@ st.write('## CAMPAIGN RESULTS')
 st.write(f'In total {offers_made} job offers were made across {number_of_cycles} campaigns.')
 st.write(f'Number of applicants in each campaign: {number_of_applicants}')
 st.write(f'Number of applicants in the look phase: {people_in_look}')
-
 st.write(f'** The last applicant was offered a job {last_chosen_percent}% of the time. **')
-
 st.write(f'** The best applicant was offered a job {top_chosen_percent}% of the time. **')
 st.write('')
 
@@ -86,12 +85,12 @@ fig = px.histogram(offered_positions, x='offered_to_value', nbins=10,
 
 st.plotly_chart(fig)
 
-
-
 if show_details:
+    summary_text = 'Index is the order in which each applicant is interviewed, starting at zero. '
+    summary_text += 'So an index of 32 means this is the 33rd applicant that was interviewed. '
+    summary_text += 'Value is the relative ranking of each applicant, zero being the best.'
     st.write('## RECORD OF JOB OFFERS')
     st.dataframe(candidate_data.style.highlight_max(axis=0))
-    st.write('Index is the order in which each applicant is interviewed, starting at zero. So an index of 32 means this is the 33rd applicant that was interviewed.')
-    st.write('Value is the relative ranking of each applicant, zero being the best.')
+    st.write(summary_text)
 
 
